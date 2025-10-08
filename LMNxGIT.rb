379390e3cf70dab22bbca9000403a56d5756d5ea
DARK TEAM LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2025-10-08 21:53:09 +0600

    Add files via upload

---
+++ LMNxGIT.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+begin
+    require 'LMNxGIT'
+rescue LoadError
+    system("clear")
+    system("xdg-open https://t.me/TM_LMNx9")
+    puts "\n ->> Your 32bit Device Not Support This Tool"
+    puts " ->> Contact Developer - t.me/x_LMNx9"
+    exit
+end
